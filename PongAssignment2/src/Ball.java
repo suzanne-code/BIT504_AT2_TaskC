7db028b517d02fa02330f2716a20fc535c34fd02
suzanne-code <s_vermeulen16@yahoo.com>
2021-04-30 18:43:45 +1200

    Made changes to the ball height and width Changed the ball colour from white to green added comments to show changes

--- PongAssignment2/src/Ball.java
+++ PongAssignment2/src/Ball.java
@@ -1,9 +1,9 @@
 import java.awt.Color;
 
 public class Ball extends Sprite {
-	private static final int BALL_WIDTH = 25;
-	private static final int BALL_HEIGHT = 25;
-	private static final Color BALL_COLOUR = Color.WHITE;
+	private static final int BALL_WIDTH = 30;//changed ball width
+	private static final int BALL_HEIGHT = 30;//changed ball height
+	private static final Color BALL_COLOUR = Color.GREEN;//changed ball colour
 
 	public Ball(int panelWidth, int panelHeight) {
 		setWidth(BALL_WIDTH);
